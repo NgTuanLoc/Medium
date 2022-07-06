79b05582e07aecb8958203c55196cb3f5f1d7bd1
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-07-06 09:55:16 +0700

    Hero page and Header completed

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
+import Link from 'next/link';
+
+function Header() {
+	return (
+		<header className='flex justify-between p-5 max-w-7xl mx-auto'>
+			<div className='flex items-center space-x-5 justify-around'>
+				<Link href='/'>
+					<img
+						className='w-44 object-conain cursor-pointer'
+						src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzVLQT1kTkaEl9pORoRyD3Pgu5LtE6IWnkHiw2FXLUYDOS56B494dH_r4ejDKzsn6cew&usqp=CAU'
+						alt='Medium Logo'
+					/>
+				</Link>
+				<div className='hidden md:inline-flex items-center space-x-5'>
+					<h3>About</h3>
+					<h3>Contacct</h3>
+					<h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>
+						Follow
+					</h3>
+				</div>
+			</div>
+			<div className='flex items-center space-x-5 text-green-600'>
+				<h3>Sign In </h3>
+				<h3 className='border px-4 py-1 rounded-full'>Get Started</h3>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,38 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Header from '../components/Header';
 import styles from '../styles/Home.module.css';
+import { FaMediumM } from 'react-icons/fa';
 
-const Home: NextPage = () => {
-	return (
-		<div className='flex flex-col items-center justify-center min-h-screen py-2'>
-			<h1>This is My Medium Page</h1>
+const Home: NextPage = () => (
+	<div>
+		<Head>
+			<title>Medium - Where good ideas find you.</title>
+			<link rel='icon' href='/favicon.ico' />
+		</Head>
+		<Header />
+
+		<div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
+			<div className='px-10 space-y-5'>
+				<h1 className='text-6xl max-w-xl font-serif'>
+					<span
+						className='underline decoration-black
+				decoration-4'>
+						Medium
+					</span>{' '}
+					is a place to write, read, and connect
+				</h1>
+				<h2>
+					It&lsquo;s easy and free to post your thinking on any topic and
+					connect with millions of readers.
+				</h2>
+			</div>
+			<div className='hidden md:inline-flex px-10'>
+				<FaMediumM className='text-[7rem] lg:text-[10rem]' />
+			</div>
 		</div>
-	);
-};
+	</div>
+);
 
 export default Home;
